7ca0223076ff2ab8132d6229d6a920f033ef2872
harrison <d.harrison.lima@gmail.com>
2025-02-11 21:55:20 -0300

    feat: add POST, PUT and DELETE methods to ProductsController

--- APICatalogo/Controllers/ProductsController.cs
+++ APICatalogo/Controllers/ProductsController.cs
@@ -1,7 +1,10 @@
 ﻿using APICatalogo.Context;
 using APICatalogo.Models;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+
 
 namespace APICatalogo.Controllers;
 
@@ -27,4 +30,61 @@ namespace APICatalogo.Controllers;
 
         return products;
         }
+
+    [HttpGet("{id:int}", Name="GetProduct")]
+
+    public ActionResult<Product> Get(int id)
+    {
+        var product = _context.Products.FirstOrDefault(p => p.ProductId == id);
+        if (product is null)
+        {
+            return NotFound("Produto não encontrado");
+        }
+
+        return product;
+
+    }
+
+    [HttpPost]
+    public ActionResult Post(Product product)
+    {
+        if(product is null)
+        {
+            return BadRequest("Produto é nulo");
+        }
+
+        _context.Products.Add(product);
+        _context.SaveChanges();
+
+        return new CreatedAtRouteResult("GetProduct", new { id = product.ProductId }, product);
+    }
+
+    [HttpPut("{id:int}")]
+    public ActionResult Put(int id, Product product)
+    {
+        if (id != product.ProductId)
+        {
+            return BadRequest("Produto não encontrado");
+        }
+
+        _context.Entry(product).State = EntityState.Modified;
+        _context.SaveChanges();
+
+        return Ok(product);
+    }
+
+    [HttpDelete("{id:int}")]
+    public ActionResult Delete(int id)
+    {
+        var product = _context.Products.FirstOrDefault(p => p.ProductId == id);
+        
+        if (product is null)
+        {
+            return NotFound("Produto não localizado...");
+        }
+        _context.Products.Remove(product);
+        _context.SaveChanges();
+
+        return Ok(product);
+    }
     }
